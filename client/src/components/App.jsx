089db5da77dc1ec122a7e6787bdab01060b3ca6e
Grant Spilsbury <grantspilsbury@gmail.com>
2018-01-24 16:16:19 +1100

    Initial commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Tutor HQ</h1>
+      </div>
+    )
+  }
+}
+
+export default App
