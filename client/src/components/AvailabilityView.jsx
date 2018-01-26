cc0ce4f1ef2146a97230086db52fdaa0a21e2d45
Zay <zayseoul@gmail.com>
2018-01-25 19:13:10 -0500

    add basic calendar

--- client/src/components/AvailabilityView.jsx
+++ client/src/components/AvailabilityView.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DayPicker from 'react-day-picker';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
 
 class AvailabilityView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.dateClick = this.dateClick.bind(this);
+    this.state = {
+      dateSelected: undefined,
+    };
+  }
+
+  dateClick(day) {
+    this.setState({ dateSelected: day })
   }
 
   render () {
     return (
       <div>
+        <DayPicker onDayClick= { this.dateClick} />
+        { this.state.dateSelected ? (
+          <p>You picked { this.state.dateSelected.toLocaleDateString()}</p>
+        ) : (
+          <p>Choose a date</p>
+        )}
+        <DayPickerInput />
       </div>
     );
   }
