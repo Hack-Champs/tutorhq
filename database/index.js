60d2fd4fc03fecc0823dccd2f072e2ee4a7d9911
Luna Kim <nayoungkimm@gmail.com>
2018-01-26 19:30:35 -0800

    Database and OAuth

--- database/index.js
+++ database/index.js
@@ -9,26 +9,39 @@ db.once('open', function() {
   console.log('Connected to mongo');
 });
 
+let bookingSchema = mongoose.Schema({
+  name: String,
+  date: String,
+  time: String
+});
+
+
+//.find({});
 let UserSchema = mongoose.Schema({
   googleId: String,
   sessionID: String,
-  username: String
-})
+  username: String,
+  email: String
+});
 
 const User = mongoose.model('User', UserSchema);
 
 const findOrCreate = (query, callback) => {
+  console.log('SAVING TO THE DATABASE');
   User.findOne({ googleID: query.googleID }, (err, user) => {
     if (!user) {
+      console.log('NEW USER');
       let newUser = new User({
         googleID: query.googleID,
         sessionID: query.sessionID,
-        username: query.username
+        username: query.username,
+        email: query.email
       });
       newUser.save(function(err, user) {
         callback(err, user);
       });
     } else {
+      console.log('RETURNING USER');
       user.sessionID = query.sessionID;
       user.save(function(err, user) {
         callback(err, user);
--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
 var db = require('../database/index.js');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
+
 require('dotenv').config();
 
 passport.serializeUser((user, done) => {
@@ -20,11 +21,13 @@ passport.deserializeUser((_id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.URL + '/auth/google/redirect',
+  callbackURL: '/auth/google/redirect',
   passReqToCallback: true
 },
   (req, accessToken, refreshToken, profile, done) => {
-    db.findOrCreate({ googleId: profile.id, sessionID: req.sessionID, username: profile.email }, function (err, user) {
+    console.log('THIS IS THE PROFILE: ', profile);
+    const username=profile.emails[0].value.slice(0, profile.emails[0].value.indexOf('@'))
+    db.findOrCreate({ googleId: profile.id, sessionID: req.sessionID, email: profile.emails[0].value, username: username }, function (err, user) {
       return done(err, user);
     });
   }
@@ -50,7 +53,7 @@ app.use(session({
 
 app.use(passport.initialize());
 
-app.get('auth/google',
+app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 app.get('/auth/google/redirect',
@@ -60,5 +63,16 @@ app.get('/auth/google/redirect',
   function(req, res) {
     res.redirect('/');
   });
+
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      res.status(501).send('Could not log out');
+    }
+    else {
+      res.status(200).send('Logged out successfully');
+    }
+  })
+})
 
 module.exports = app;
