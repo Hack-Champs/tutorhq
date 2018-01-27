cb1f8e99d4481e2bc21e668d759dc60143bd718e
Luna Kim <nayoungkimm@gmail.com>
2018-01-26 19:40:21 -0800

    database and oauth

--- database/index.js
+++ database/index.js
@@ -21,10 +21,27 @@ let UserSchema = mongoose.Schema({
   googleId: String,
   sessionID: String,
   username: String,
+  description: String,
+  subjects: [String],
+  tutor: String,
   email: String
 });
 
+//way to update the subject (PUT)
+//GET to return info of the specific tutor based on username
+//GET all tutors
+
+let ratingSchema = mongoose.Schema({
+  tutorId: Number,
+  rating: Number,
+});
+
+//sum of ratings divided by the number of records
+//average rating
+
 const User = mongoose.model('User', UserSchema);
+
+const Booking = mongoose.mode('Booking', bookingSchema);
 
 const findOrCreate = (query, callback) => {
   console.log('SAVING TO THE DATABASE');
