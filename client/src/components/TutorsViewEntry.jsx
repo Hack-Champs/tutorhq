ca02a8dbd9da55c430da6fe1ce3ab1251954b0f5
Henry Neale <hackerhneale@gmail.com>
2018-01-31 22:28:23 -0500

    Resolve merge conflicts

---
+++ client/src/components/TutorsViewEntry.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import { Item } from 'semantic-ui-react';
+
+const TutorsViewEntry = ({ tutor }) => (
+  <Item as={Link} to={`/tutors/${tutor.username}`} replace>
+    <Item.Content header={tutor.name} description={tutor.description} />
+  </Item>
+);
+
+export default TutorsViewEntry;
