caa4e3c479f6b60bf6d093cc7436c0b0f617f30f
Grant Spilsbury <grantspilsbury@gmail.com>
2018-02-10 13:30:16 +1100

    Ignore pems

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -7,8 +7,7 @@ class Video extends Component {
     this.webrtc = new SimpleWebRTC({
       localVideoEl: 'localVideo',
       remoteVideosEl: 'remoteVideos',
-      autoRequestMedia: true,
-      url: 'https://tutorhq.herokuapp.com'
+      autoRequestMedia: true
     });
     this.startVideo = this.startVideo.bind(this);
     this.toggleVideo = this.toggleVideo.bind(this);
