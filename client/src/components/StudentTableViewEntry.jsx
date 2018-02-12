02c5e8ec6919fb8265f0db05fffdefcfb728cc3c
Henry Neale <hackerhneale@gmail.com>
2018-02-12 17:38:51 -0500

    Reformat student table and student table entries

--- client/src/components/StudentTableViewEntry.jsx
+++ client/src/components/StudentTableViewEntry.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Table } from 'semantic-ui-react';
 
-const StudentTableViewEntry = ({ session, i }) => {
+const StudentTableViewEntry = ({ session, student, booking, i }) => {
   return (
     <Table.Row>
-      <Table.Cell>{ session.date }</Table.Cell>
-      <Table.Cell>{ session.time }</Table.Cell>
+      <Table.Cell>{student}</Table.Cell>
+      <Table.Cell>{booking[student].email}</Table.Cell>
+      <Table.Cell>{`${session.date}, ${session.time}`} </Table.Cell>
+      <Table.Cell>Notes: {booking[student].notes}</Table.Cell>
       {/* <Table.Cell>Session Length: { session.time.billableTime }</Table.Cell> */}
     </Table.Row>
   );
